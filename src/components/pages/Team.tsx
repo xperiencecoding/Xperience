fb59bd9368aad99ce246a4df5f4e93fd642abff3
Lin <lol96297@gmail.com>
2024-06-14 16:34:48 -0500

    test update

--- src/components/pages/Team.tsx
+++ src/components/pages/Team.tsx
@@ -377,7 +377,7 @@ function Team(props: {}) {
 
                             <div key="g">
                                 <div className="profile">
-                                    <img src={require("../../img/Abraham.png")} />
+                                    <img src={require("../../img/abe.jpg")} />
                                     <figcaption className="name oxanium bold">
                                         Abraham Guo
                                         <div className="icons">
