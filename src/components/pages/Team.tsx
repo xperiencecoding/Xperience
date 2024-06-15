ffe0f082e7ac01018d93583dbf611f9c2dc22ca7
xperiencecoding <xperienced128@gmail.com>
2024-06-15 15:05:54 -0500

    changes

--- src/components/pages/Team.tsx
+++ src/components/pages/Team.tsx
@@ -377,7 +377,7 @@ function Team(props: {}) {
 
                             <div key="g">
                                 <div className="profile">
-                                    <img src={require("../../img/abe.jpg")} />
+                                    <img src={require("../../img/Abrahamm.jpg")} />
                                     <figcaption className="name oxanium bold">
                                         Abraham Guo
                                         <div className="icons">
